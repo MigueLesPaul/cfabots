b25a398984a24ceb370ddef78bc4990e3390c50b
MigueLEsPaul <miguel.hinojosa@insmet.cu>
2020-09-26 21:26:44 -0400

    MOD: Generar el video usando imageio

--- scripts/imgconvert.py
+++ scripts/imgconvert.py
@@ -4,13 +4,21 @@ import os
 from os.path import basename
 from glob import glob
 import re
-
+from imageio import imread,mimwrite,mimsave,get_writer
+from imageio_ffmpeg import write_frames
 
 
 def natural_sort(l): 
     convert = lambda text: int(text) if text.isdigit() else text.lower() 
     alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
     return sorted(l, key = alphanum_key)
+
+def read_images(flist):
+    imglist=[]
+    for f in flist:
+        imglist.append(imread(f))
+    return imglist
+
 
 def pngs2mp4(fileexpr, imagesize='640:480'):
     """ 
@@ -20,10 +28,21 @@ def pngs2mp4(fileexpr, imagesize='640:480'):
     newfilename = fileexpr[:-1]
 
     filelist = natural_sort(glob(fileexpr+'.png'))
+
+    mimsave(newfilename+".gif",read_images(filelist))
+    
+    vidwriter = get_writer(newfilename+".mp4",format='FFMPEG',fps=1,codec='h264',bitrate='20k')
+
+    for img in read_images(filelist):
+        print(img.shape)
+        vidwriter.append_data(img)
+    vidwriter.close()
+
+
     
 
-    cmd1 = """cat {}|/usr/bin/ffmpeg -y -framerate 1 -i -  -vcodec libvpx -b:v 100k -s 640x480  "{}.webm" """.format(
-    " ".join(filelist), newfilename)
+#    cmd1 = """cat {}|/usr/bin/ffmpeg -y -framerate 1 -i -  -vcodec libvpx -b:v 100k -s 640x480  "{}.webm" """.format(
+#    " ".join(filelist), newfilename)
 
 
     # cmd1 = """/usr/bin/ffmpeg -y  -framerate 1/2.5 -i {}  -vcodec libvpx -b:v 100k -s 640x480  -r 25 -pix_fmt yuv420p "{}.webm" """.format(
@@ -34,8 +53,8 @@ def pngs2mp4(fileexpr, imagesize='640:480'):
     # cmd2 = """/usr/bin/ffmpeg -y  -i {}.webm  -b:v 8k -s 640x480 "{}.gif" """.format(
         # fileexpr, newfilename)
 
-    print(cmd1)
-    os.system(cmd1)
+#    print(cmd1)
+#    os.system(cmd1)
     # os.system(cmd2)
     return """{}.webm""".format(newfilename)
 
@@ -46,7 +65,7 @@ if __name__ == "__main__":
     # imagesize='640x480')
 
     vidfile = pngs2mp4(
-        "/home/miguel/Projects/cfabots/images/wrfout_2020082806/SFC/RAIN/wrfout_2020082806_d3_rain_sfc_*",
+        "/home/miguel/Data/SispiImages/wrfout_2020082800/SFC/RAIN/wrfout_2020082800_d3_rain_sfc_*",
         imagesize='640:480')
 
     # pngs2mp4('/home/miguel/Pictures/.webcam/20200827/shot*')
