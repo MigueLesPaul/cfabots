9a2203b12edc5ceb8273b8476faee4e51aa504f1
MigueLEsPaul <miguelhinojosa994@gmail.com>
2020-10-16 23:54:56 -0400

    ADD: Un Bot para reunirlos a todos y atarlos a las tinieblas 👿 	new file:   CFABot.py

---
+++ CFABot.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import os
+from os.path import expanduser
+import json
+import time
+from TelBot import TelBot
+from FaceBot import Facebook
+from TwittBot import TwitBot
+
+
+# Centro de Física de la Atmósfera 2020 
+# Publicar salidas de SisPI en las redes sociales 
+# Miguel 
+# Ariel 
+# Adrián 
+
+
+class CFABot(TelBot,Facebook,TwitBot):
+	""" 
+	Publicar salidas de SisPI en las redes sociales
+	Hereda de las clases especificas para cada una de las redes sociales utilizadas por rel CFA 
+	"""
+	def __init__(self):
+		credentials = json.load(open(expanduser("~/credentials.json"),'r'))
+
+
+		super(TelBot, self  ).__init__(credentials['Telegram Bot Token'])
+		self.channel_id=credentials['Telegram channel_id']
+
+		# super(Facebook, self).__init__(p_page_id=credentials["Facebook Page Id"] ,p_page_access_token=credentials['Facebook Page Token'])
+		Facebook.__init__(self,p_page_id=credentials["Facebook Page Id"] ,p_page_access_token=credentials['Facebook Page Token'])
+		
+		super(TwitBot, self  ).__init__()
+
+
+
+	def post_msg(self,msg):
+		pass	
+	def post_img(self,msg,imgfile):
+		pass
+	def post_vid(self,msg,vidfile):
+		
+		# Telegram
+		try:
+			response=self.sendVideo(self.channel_id,
+                             video=open(vidfile, 'rb'),
+                             width=480,
+                             height=320,
+                             caption=msg)
+			print(response)
+		except:
+			pass
+
+		# Twitter	
+		try:
+			response = self.postUpdate(vidfile,msg)
+			print(response)
+		except:
+			pass
+
+		# Facebook	
+		try:
+			response=self.post_video(vidfile,msg)
+			print(response)
+		except:
+			pass
+
+
+
+# if __name__ == '__main__':
+# 	bot = CFABot()	
+# 	bot.post_vid('Post de prueba de bot Facebook','/home/miguel/Projects/cfabots/wrfout_2020082806_d3_rain_sfc_.mp4')	
